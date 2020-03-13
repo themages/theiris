ae490a4b70879e7739313db28e6c92ed7a025238
zengxingqi <me@zengxingqi.com>
2020-03-14 02:06:16 +0800

    add: app lock add windows show/close

--- app/main/index.js
+++ app/main/index.js
@@ -1,10 +1,24 @@
 const { app } = require("electron");
 const handleIPC = require("./ipc.js");
-const { create: createMainWindow } = require("./windows/main");
+const { create: createMainWindow, show: showMainWindow, close: closeMainWindow } = require("./windows/main");
 // const { create: createControlWindow } = require("./windows/control");
-app.on("ready", () => {
-    createMainWindow();
-    // createControlWindow();
-    handleIPC();
-    require("./robot")();
-});
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+    app.quit();
+} else {
+    app.on("second-instance", (event, commandLine, workingDirectory) => {
+        showMainWindow();
+    });
+    app.on("ready", () => {
+        createMainWindow();
+        // createControlWindow();
+        handleIPC();
+        require("./robot")();
+    });
+    app.on("before-quit", () => {
+        closeMainWindow();
+    });
+    app.on("activate", () => {
+        showMainWindow();
+    })
+}
--- app/main/windows/main.js
+++ app/main/windows/main.js
@@ -2,14 +2,23 @@ const { BrowserWindow } = require("electron");
 const isDev = require("electron-is-dev");
 const path = require("path");
 let win;
+let willQuitApp = false;
 function create() {
     win = new BrowserWindow({
-        width: 1200,
-        height: 960,
+        width: 600,
+        height: 480,
         webPreferences: {
             nodeIntegration: true
         }
     });
+    win.on("close", (e) => {
+        if (willQuitApp) {
+            win = null;
+        } else {
+            e.preventDefault();
+            win.hide();
+        }
+    })
     if (isDev) {
         win.loadURL("http://localhost:3000");
     } else {
@@ -23,4 +32,13 @@ function send(channel, ...args) {
     win.webContents.send(channel, ...args);
 }
 
-module.exports = { create, send };
+function show () {
+    win.show();
+}
+
+function close () {
+    willQuitApp = true;
+    win.close();
+}
+
+module.exports = { create, send, show, close };
