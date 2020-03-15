d384068a18b028da873cdd1c7ddebcede9ad0e42
zengxingqi <me@zengxingqi.com>
2020-03-15 23:24:29 +0800

    update: package

--- app/main/dll/activate-windows.js
+++ app/main/dll/activate-windows.js
@@ -4,7 +4,7 @@ const user = new FFI.Library("user32", {
     "FindWindowA": ["int32", ["string", "string"]],
     "ShowWindow": ["int32", ["int32", "int32"]]
 });
-console.log(user)
+
 function showWeChat() {
     let res = user.FindWindowA("WeChatMainWndForPC", null);
     let show = user.ShowWindow(res, 5);
