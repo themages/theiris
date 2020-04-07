52a8c7ecf5ebbafe9f3dfb21f91b9a8c44dc5463
zengxingqi <me@zengxingqi.com>
2020-04-08 00:07:36 +0800

    update: pack

--- app/renderer/src/main/build.js
+++ app/renderer/src/main/build.js
@@ -1,4 +1,10 @@
 const fs = require("fs-extra");
 const dist = "../../pages/main";
+const myDir = "./build";
 fs.removeSync(dist);
-fs.moveSync("./build", dist);
+fs.access(myDir, function(err) {
+    if (err && err.code === 'ENOENT') {
+        fs.mkdir(myDir);
+    }
+});
+fs.moveSync(myDir, dist);
